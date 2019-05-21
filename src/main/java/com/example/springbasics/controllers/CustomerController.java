7f36e99984bc29f2a3255b2466c18a86dd6ddb20
rahmakevo <siundu344@gmail.com>
2019-05-21 09:08:11 +0300

    working on a simple post request

--- src/main/java/com/example/springbasics/controllers/CustomerController.java
+++ src/main/java/com/example/springbasics/controllers/CustomerController.java
@@ -3,9 +3,7 @@ package com.example.springbasics.controllers;
 import com.example.springbasics.model.Customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,17 +11,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
+
     private static int customerId = 0;
     private static List<Customer> customer = new ArrayList<>();
-    @GetMapping
+
+    @GetMapping("/customers/get")
     public ResponseEntity <List<Customer>> getCustomers() {
         Customer mCustomer = new Customer();
         mCustomer.setId(String.valueOf(customerId));
         mCustomer.setFirstName("FirstName "+ customerId);
         mCustomer.setLastName("LastName "+ customerId);
-
         customer.add(mCustomer);
         return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @PostMapping("/customers/create")
+    public ResponseEntity<Customer> createCustomer (@RequestBody Customer customers) {
+        customers.setId(String.valueOf(customerId));
+        customer.add(customers);
+        return new ResponseEntity<>((Customer) customer, HttpStatus.OK);
     }
 
 }
