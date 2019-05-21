06d68f1e3b9f39d32d1984583b5a84b457aa6789
rahmakevo <siundu344@gmail.com>
2019-05-21 15:29:19 +0300

    working on refactoring code

--- src/main/java/com/example/springbasics/repository/CustomerRepository.java
+++ src/main/java/com/example/springbasics/repository/CustomerRepository.java
@@ -3,9 +3,7 @@ package com.example.springbasics.repository;
 import com.example.springbasics.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 @Repository
-@Service(value = "CustomerRepository")
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 }
