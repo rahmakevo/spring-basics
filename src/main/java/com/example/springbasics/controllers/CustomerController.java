8c05bab32d01ca45bc4137ebf77f45541b3e7228
rahmakevo <siundu344@gmail.com>
2019-05-21 09:41:50 +0300

    working on cleaning code

--- src/main/java/com/example/springbasics/controllers/CustomerController.java
+++ src/main/java/com/example/springbasics/controllers/CustomerController.java
@@ -5,20 +5,21 @@ import com.example.springbasics.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/customers")
 public class CustomerController {
 
-    @Autowired
-    private CustomerRepository repository;
-
     private static int customerId = 0;
     private static List<Customer> customer = new ArrayList<>();
+
+    @Autowired
+    CustomerRepository repository;
 
     @GetMapping("/customers/get")
     public ResponseEntity <List<Customer>> getCustomers() {
