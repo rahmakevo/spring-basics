aa8ae66e34b7fbddfd898c047f944c75bfe21a37
rahmakevo <siundu344@gmail.com>
2019-05-21 09:53:02 +0300

    working on refactoring code

--- src/main/java/com/example/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/example/springbasics/SpringBasicsApplication.java
@@ -3,7 +3,7 @@ package com.example.springbasics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.springbasics.controllers", "com.example.springbasics.model", "com.example.springbasics.repository"})
 public class SpringBasicsApplication {
 
 	public static void main(String[] args) {
