c61273ae91eea886bb9cfbe3d31aaab07422e42d
rahmakevo <siundu344@gmail.com>
2019-05-21 11:04:08 +0300

    cleaning code

--- src/main/java/com/example/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/example/springbasics/SpringBasicsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.example.springbasics.repository")
 public class SpringBasicsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/springbasics/repository/CustomerRepository.java
+++ src/main/java/com/example/springbasics/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.example.springbasics.repository;
 
 import com.example.springbasics.model.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 }
