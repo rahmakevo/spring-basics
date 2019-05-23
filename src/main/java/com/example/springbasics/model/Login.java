b4129b0f8bf92406cb4898e1c603e9966cbe1229
rahmakevo <siundu344@gmail.com>
2019-05-23 14:20:16 +0300

    working on user registration and login

--- src/main/java/com/example/springbasics/model/Login.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbasics.model;
-
-import lombok.Data;
-
-@Data
-public class Login {
-    public String username;
-    public String password;
-}
--- src/main/java/com/example/springbasics/model/Token.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbasics.model;
-
-import lombok.Data;
-
-@Data
-public class Token {
-    String token;
-    Long expTime;
-}
