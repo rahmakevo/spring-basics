e3cb8d928d5c9ed028b301300e11a0dc18ada90b
rahmakevo <siundu344@gmail.com>
2019-05-23 15:49:08 +0300

    securing endpoints with jwt

--- src/main/java/com/example/springbasics/security/SecurityConfig.java
+++ src/main/java/com/example/springbasics/security/SecurityConfig.java
@@ -58,5 +58,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
