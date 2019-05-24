da2b3fa13a524fd677f436ac1a4c6b45575141df
rahmakevo <siundu344@gmail.com>
2019-05-24 10:48:00 +0300

    pushing to github

--- src/main/java/com/example/springbasics/controller/AuthController.java
+++ src/main/java/com/example/springbasics/controller/AuthController.java
@@ -23,7 +23,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.Collections;
 
-@RestController("/basics")
+@RestController("/api/auth")
 public class AuthController {
 
     @Autowired
