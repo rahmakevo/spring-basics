6b838667f716690a1902840a37ea250135b5b453
rahmakevo <siundu344@gmail.com>
2019-05-25 17:52:24 +0300

    working on authorizing certain endpoints

--- src/main/java/com/example/springbasics/security/SecurityConfig.java
+++ src/main/java/com/example/springbasics/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**")
+                .antMatchers("/api/auth/signup")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
