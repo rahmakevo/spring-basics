89edc8289673ce6435468280168c8a490dd681a4
rahmakevo <siundu344@gmail.com>
2019-05-25 22:39:43 +0300

    working on refactoring code

--- src/main/java/com/example/springbasics/security/SecurityConfig.java
+++ src/main/java/com/example/springbasics/security/SecurityConfig.java
@@ -70,6 +70,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/api/auth/signup")
+                .permitAll()
+                .anyRequest()
                 .authenticated();
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
