1e6d748b738a031c6e4f7a9305b227873adc5b6e
rahmakevo <siundu344@gmail.com>
2019-05-27 11:36:15 +0300

    working on auth config

--- src/main/java/com/example/springbasics/security/SecurityConfig.java
+++ src/main/java/com/example/springbasics/security/SecurityConfig.java
@@ -58,18 +58,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.cors()
-                .and()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+        http.cors().and()
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**/api/auth/signup")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
+                .antMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
